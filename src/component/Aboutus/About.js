19f715441f167ea98d03b2398fd61e95643ea5ca
akansha8867 <akansharana2000@gmail.com>
2020-10-03 14:38:37 +0530

    mobile view

--- src/component/Aboutus/About.js
+++ src/component/Aboutus/About.js
@@ -5,7 +5,8 @@ import Process from '../images/Process.png';
 import Objective from '../images/Objective.png';
 
 function About() {
-        return (
+    return (
+        <div>
             <div className='aboutContainer'>
                 <div className='aboutHeading'>
                     <h1>About Us</h1>
@@ -39,7 +40,43 @@ function About() {
                     </div>
                 </div>
             </div>
-        )
+
+            <div className='mobile-aboutContainer'>
+                <div className='mobile-aboutHeading'>
+                    <h1>About Us</h1>
+                    <p>Be Digital ready with our Fast and Cost-effective Services.</p>
+                </div>
+                <div className="mobile-aboutObjective">
+                    <div className="mobile-objectiveLeft">
+                        <h1>What's Our Objective?</h1>
+                        <div className="mobile-objectiveRight">
+                            <img src={Objective} alt="WhatWeDo" />
+                        </div>
+                        <p>We understand the need of getting digital in today’s world. We as a team are willing to give a boost up to businesses as well as start-ups on digital platform. RACTS prefer giving equal importance to all our clients regardless of type. All our work is customised to the needs of our clients and we do our best to fulfil client’s requirement. We are available 24/7 to answer all your queries.</p>
+                    </div>
+                </div>
+                <div className="mobile-aboutWeDo">
+                    <div className="mobile-aboutRight">
+                        <h1>What do we do?</h1>
+                        <p>RACTS helps you to grow your business by providing you a website, mobile application or revamp your existing one at power with the advancements in digital world. We provide our clients the best solutions as early as possible.</p>
+                    </div>
+                    <div className="mobile-aboutLeft">
+                        <img src={WhatWeDo} alt="WhatWeDo" />
+                    </div>
+                </div>
+                <div className="mobile-aboutProcess">
+                    <div className="mobile-processLeft">
+                        <h1>What's our Process?</h1>
+                        <div className="mobile-processRight">
+                            <img src={Process} alt="Process" />
+                        </div>
+                        <p>We here understand that IT services are changing and are willing to provide our clients with the best ones. We proceed by understanding client’s idea and work with proper planning and precision. Visual representation of the client’s imagination helps them to see their idea clearly. RACTS believes that the product delivered to the client should be genuine and we run full testing of the products to ensure the same.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About;
--- src/component/Projects/Project.js
+++ src/component/Projects/Project.js
@@ -5,12 +5,13 @@ import Barbara from '../images/Templates/3.png';
 import './Project.css';
 
 function Project() {
-        return (
+    return (
+        <div>
             <div className='projectContainer' id='ProjectsContainer'>
                 <div className='extraSpace'></div>
                 <div className='projectHeading'>
                     <h1>Our Work</h1>
-                    <p>Be Digital ready with our Fast and Cost-effective Services.</p>
+                    <p>Brief overview of our work</p>
                 </div>
                 <div className="projectCards">
                     <div className="card1">
@@ -45,7 +46,50 @@ function Project() {
                     <button className='SeeMoreBtn'>See More</button>
                 </div>
             </div>
-        );
+
+            <div className='mobile-projectContainer' id='ProjectsContainer'>
+                <div className='extraSpace'></div>
+                <div className='mobile-projectHeading'>
+                    <h1>Our Work</h1>
+                    <p>Brief overview of our work</p>
+                </div>
+                <div className="mobile-projectCards">
+                    <div className="mobile-card1">
+                        <div className="mobile-cardImg">
+                            <img src={Adopto} alt="FoodDelight" />
+                        </div>
+                        <div className="mobile-cardDetails">
+                            <h1>Adopto</h1>
+                            <p>A pet lover knows what pet adoption means and here's a look of our very own pet adoption website.</p>
+                        </div>
+                    </div>
+                    <div className="mobile-card2">
+                        <div className="mobile-cardImg">
+                            <img src={Medico} alt="Lolios" />
+                        </div>
+                        <div className="mobile-cardDetails">
+                            <h1>Medico</h1>
+                            <p>Medico app allows video consultation with india's top doctors connect from anywhere anytime from ayurveda to child care.</p>
+                        </div>
+                    </div>
+                    <div className="mobile-card3">
+                        <div className="mobile-cardImg">
+                            <img src={Barbara} alt="Covid" />
+                        </div>
+                        <div className="mobile-cardDetails">
+                            <h1>Barbara</h1>
+                            <p>We here know hair can be a tough thing to understand  and different products, Barbara offers a variety of styles that'd match your personality.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectsMore">
+                    <button className='SeeMoreBtn'>See More</button>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default Project;
